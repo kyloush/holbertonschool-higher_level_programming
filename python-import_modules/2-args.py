f2697b72d32aac6368a704973df1527b0fdde55f
kyloush <kylegujadur@gmail.com>
2025-07-09 11:34:13 +0400

    s

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     elif arg_count == 1:
         print('1 argument:')
     else:
-        print("{} argument:".format(arg_count))
+        print("{} arguments:".format(arg_count))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
