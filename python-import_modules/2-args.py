eaa1293a4a576af814cc6d6e08f1f4a3faee67db
kyloush <kylegujadur@gmail.com>
2025-07-09 11:31:37 +0400

    argcount

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     arg_count = len(sys.argv) - 1
     if arg_count == 0:
         print("0 arguments.")
-    elif arg == 1:
+    elif arg_counts == 1:
         print('1 argument:')
     else:
         print("{} argument:".format(arg_count))
