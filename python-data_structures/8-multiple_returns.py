894471b60b9209f587d1b9f45c7f71e96b673a3f
kyloush <kylegujadur@gmail.com>
2025-07-14 21:35:25 +0400

    pp

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first = sentence[0]
-    return(length, first)
+    length = len(sentence) if sentence else 0
+    first = sentence[0] if sentence else none
+    return (length, first)
