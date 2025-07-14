2864bb20b927584d2d04a733e74f9dd37e2c2166
kyloush <kylegujadur@gmail.com>
2025-07-14 21:19:58 +0400

    tkt

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple_a + tuple_b
+    a1 = tuple_a[0]
+    a2 = tuple_a[1]
+    b1 = tuple_b[0]
+    b2 = tuple_b[1]
+    return (a1 + b1, a2 + b2)
