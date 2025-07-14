864ede46396afdb4c34509d8724590072dd0987a
kyloush <kylegujadur@gmail.com>
2025-07-14 21:57:00 +0400

    yoy

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for num in my_list:
+        if num % 2 == 0:
+            print(f"{num} is divisible by {2}")
+        else:
+            print(f"{num} is not divisible by {2}")
+    return (my_list)
