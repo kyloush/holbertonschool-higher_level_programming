4df3e3e663195f3f03d80af42a5f641df01607ac
kyloush <kylegujadur@gmail.com>
2025-07-14 21:06:01 +0400

    lol

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     result = ""
     for char in my_string:
-        if ord(char) != 99 and ord(char) != 67:  # 99 = 'c', 67 = 'C'
+        if ord(char) != 99 and ord(char) != 67:
             result += char
     return result
